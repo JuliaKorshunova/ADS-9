8a3ab4423ebb0509c0d4c74ea2ee0fd9c3c337a7
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2023-05-29 19:50:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::string war;
+    BST<std::string> peace;
+    std::ifstream file(filename);
+    if (file) {
+        while (!file.eof()) {
+            char words = toolower(file.get());
+            if (!alpha(words)) {
+                if (!war.empty()) {
+                    peace.add(war);
+                }
+                war.clear();
+            } else {
+                war += words;
+            }
+        }
+    } else {
+        std::cout << "Error file!" << std::endl;
+    }
+    file.close();
+    return peace;
 }
